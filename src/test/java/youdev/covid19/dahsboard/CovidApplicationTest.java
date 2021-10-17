462121325abc3b4a0106f8d3791fb47799bfca42
youdev97 <younes.blcd@gmail.com>
2021-10-17 13:43:25 +0200

    test package and class fixed

---
+++ src/test/java/youdev/covid19/dahsboard/CovidApplicationTest.java
@@ -0,0 +1,14 @@
+package youdev.covid19.dahsboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CovidApplicationTest {
+
+	@Test
+	void test() {
+		
+	}
+
+}
