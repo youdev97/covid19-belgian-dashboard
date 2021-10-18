9546949a9d957d41fbe4179efec63a8ab427718a
youdev97 <younes.blcd@gmail.com>
2021-10-18 11:26:36 +0200

    remove unecessary annotations

--- src/main/java/youdev/covid19/dahsboard/controller/HospitalsDataController.java
+++ src/main/java/youdev/covid19/dahsboard/controller/HospitalsDataController.java
@@ -3,10 +3,8 @@ package youdev.covid19.dahsboard.controller;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonParseException;
@@ -23,8 +21,8 @@ public class HospitalsDataController {
 	private DataServiceImpl service;
 
 	
-	@GetMapping(value = {""}, produces = MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody String getHospitalsData() throws JsonParseException, IOException {
+	@GetMapping(value = {""})
+	public String getHospitalsData() throws JsonParseException, IOException {
 		log.info("/api/hospitals call get hospitals data");
 		String json = service.getLastData();
 		return json;
